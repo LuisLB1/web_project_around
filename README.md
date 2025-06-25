cfcb0cf1c26055295312ed73d3fc33dd5f233021
Otay Warehouse <jmwarehouse@jensenmeat.com>
2025-06-24 22:32:05 -0700

    act6/24w

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Tripleten web_project_around
+
+Este proyecto se enfoco en poner en practica nuestros primeros conocimientos sobre JavaScript, realizando un Popup y agregar funcionalidades que modifiquen directamente elementos del html.
+ Se utilizo el llamda a las constantes "const", funciones "function()", y selectores de elementos en el DOM, entre otros.
+ 
---
+++ pages/index.js
@@ -0,0 +1,35 @@
+document.addEventListener("DOMContentLoaded", function(){
+
+const openPopup = document.querySelector('.main__profile-button');
+const closePopup = document.querySelector('.popup-close');
+const overlayPopup = document.querySelector('.popup-overlay');
+
+
+const inputAcercademi = document.getElementById('acercaDemipopup');
+const pAcercademi = document.getElementById('acercaDemi');
+
+const nameAuthor = document.getElementById('nameJ');
+const popupName = document.getElementById('inputName');
+
+const btnGuardar = document.getElementById('guardarBtn');
+
+openPopup.addEventListener("click", function() {
+overlayPopup.style.display = "flex";
+});
+closePopup.addEventListener("click", function() {
+overlayPopup.style.display = "none";
+});
+
+btnGuardar.addEventListener("click", function(){
+    const nuevoNombre = popupName.value.trim();
+    const nuevoTexto = inputAcercademi.value.trim();
+
+    nameAuthor.textContent = nuevoNombre || "Jacques Cousteau";
+    pAcercademi.textContent = nuevoTexto || "Explorador";
+
+    overlayPopup.style.display = "none";
+
+
+})
+
+});
