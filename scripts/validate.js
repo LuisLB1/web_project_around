d01bdf6a5b86df18ac4cdc5713bea67822189664
Otay Warehouse <jmwarehouse@jensenmeat.com>
2025-08-01 02:01:44 -0700

    validar datos primera parte

---
+++ scripts/validate.js
@@ -0,0 +1,4 @@
+const primerFormulario = document.querySelector(".popup__form")
+const nombreFormulario = document.getElementById("inputName");
+const acercaDemiFormulario = document.getElementById("acercaDemipopup");
+const guardarBtnFormulario = document.getElementById("guardarBtn");
